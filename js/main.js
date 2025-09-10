f5c264f515ba063a964c3ec86ec93041752e863b
OlegKrivenko <krivenko.olegus@gmail.com>
2025-09-11 00:21:21 +0200

    created dark theme

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-btnDarkMode.onclick = function () {
-  console.log(btnDarkMode);
-  btnDarkMode.classList.toggle("dark-mode-btn--active");
-};
+// Проверка наличия кнопки на странице
+if (btnDarkMode) {
+  // Проверка сохраненнрй темы или системной (в виндовс или браузере)
+  // в localStorage при загрузке страницы
+  const savedTheme = localStorage.getItem("darkMode");
+  const prefersDarkScheme = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  if (savedTheme === "dark" || (!savedTheme && prefersDarkScheme)) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+  }
+
+  // Включение ночного режима по кнопке и запись в localStorage
+  btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+    localStorage.setItem("darkMode", isDark ? "dark" : "light");
+  };
+}
