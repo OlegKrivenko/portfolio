a85381f9060fbe7ec09ed628f311584895bc4fd2
OlegKrivenko <krivenko.olegus@gmail.com>
2025-09-19 08:55:12 +0200

    changed main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
 if (btnDarkMode) {
+  // Проверка настроек пользователя в браузере
   const savedTheme = localStorage.getItem("darkMode");
+  // Проверка системных настроек пользователя на компе
   const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
   console.log(prefersDarkScheme);
   // Установка начальной темы
@@ -13,14 +15,18 @@ if (btnDarkMode) {
   document.body.classList.toggle("dark", shouldUseDark);
 
   // Слушаем переключение системной темы в опереционной системе пользователя (если пользователь не выбрал вручную)
+  // За сутки на компе тема в зависимости от времени может меняться
   prefersDarkScheme.addEventListener("change", (e) => {
-    if (!localStorage.getItem("darkMode")) {
-      btnDarkMode.classList.toggle("dark-mode-btn--active", e.matches);
-      document.body.classList.toggle("dark", e.matches);
+    if (e.matches) {
+      document.body.classList.add("dark");
+      btnDarkMode.classList.add("dark-mode-btn--active");
+    } else {
+      document.body.classList.remove("dark");
+      btnDarkMode.classList.remove("dark-mode-btn--active");
     }
   });
 
-  // Переключение темы по кнопке
+  // Переключение темы по кнопке в браузере
   btnDarkMode.onclick = function () {
     const isDark = document.body.classList.toggle("dark");
     btnDarkMode.classList.toggle("dark-mode-btn--active", isDark);
